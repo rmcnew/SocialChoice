116a49954a580094962b19d34bc80a9542a0d5d2
Richard Scott McNew <richard.scott.mcnew@gmail.com>
2019-03-04 20:31:11 -0700

    Input table structure done

---
+++ script.js
@@ -0,0 +1,22 @@
+/*
+CS 6110 Program 3
+Richard Scott McNew
+richard.scott.mcnew@gmail.com
+A02077329
+    
+ Copyright (C) 2019, Richard Scott McNew.  
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/ 
